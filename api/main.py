c14f036f1735009ed34f2ebad036399aca5ffd50
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-01-19 12:23:06 +0300

    feature: Multiclass output

--- api/main.py
+++ api/main.py
@@ -16,20 +16,34 @@ def hello():
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    def run_model(model, X, k):
+        import numpy as np
+
+        all_probs = model.predict_proba(X)
+        topk_idx = np.argpartition(y, -k)[:, :-k-1:-1]
+        topk_classes = model.classes_[topk_idx]
+        topk_probs = np.array([all_probs[n, idx]
+                               for n, idx in enumerate(topk_idx)])
+        return [
+            [{'class': c, 'probability': p}
+             for p, c in sorted(zip(probs, classes), reverse=True)]
+            for classes, probs in zip(topk_classes, topk_probs)]
+
     if model:
         try:
             json_ = request.json
             print(json_)
             title = TextPreprocessor.clean_text(json_['title'])
             body = TextPreprocessor.clean_text(json_['body'])
+            k = json_.get('k', 3)
 
             query = title + ' ' + body
-            prediction = model.predict([query])[0]
+            prediction = run_model(model, [query], k)[0]
 
             return jsonify({
                 'title': title,
                 'body': body,
-                'prediction': str(prediction)})
+                'prediction': prediction})
 
         except:
             return jsonify({'trace': traceback.format_exc()})
